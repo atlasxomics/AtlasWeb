de59aaccee257219024885cfe2b64dd384fde419
Jonahs11 <jonahsilverman11@gmail.com>
2022-07-20 08:31:39 -0400

    Loading Runs on screen as buttons upon click of run

--- src/views/Home/components/tools/atlasRunViewer/AtlasRunViewer.vue
+++ src/views/Home/components/tools/atlasRunViewer/AtlasRunViewer.vue
@@ -4,22 +4,27 @@
           <v-col>
             <h1> Run IDs </h1>
             <v-spacer></v-spacer>
-            <button @click="getRunFiles">D223</button>
+            <button @click="getRunFiles('D223')">D223</button>
           </v-col>
           <v-col>
             <h1>
               Options
             </h1>
+            <button v-for="file in availableFiles" :key="file"
+            @click="loadFile(file)"
+            >
+            {{file}}
+            </button>
           </v-col>
         </v-row>
     </v-container>
 </template>
 
 <script lang='ts'>
-import { defineComponent, computed } from '@vue/composition-api';
+import { defineComponent, computed, ref } from '@vue/composition-api';
 import store from '../../../../../store';
 
-const client_Ready = new Promise((resolve) => {
+const clientReady = new Promise((resolve) => {
   const ready = computed(() => (
     store.state.client !== null
   ));
@@ -29,19 +34,30 @@ export default defineComponent({
   props: ['query'],
   setup(props, ctx) {
     const client = computed(() => store.state.client);
-    async function getRunFiles() {
+    const availableFiles = ref<any[]>([]);
+    // getRunFiles
+    async function getRunFiles(runID: string) {
       if (!client.value) {
         return;
       }
-      const runID = 'D223';
+      // const runID = 'D223';
       const folder_path = 'data/'.concat(runID);
       const file_payload = { params: { path: folder_path } };
-      console.log(file_payload);
-      const files = await client.value.getFileList(file_payload);
-      console.log(files);
-      console.log('Getting files for '.concat(runID));
+      const run_files = await client.value.getFileList(file_payload);
+      availableFiles.value = run_files;
+      if (availableFiles.value.length === 0) {
+        availableFiles.value.push('Run '.concat(runID).concat(' has no associated files.'));
+      }
+      console.log(availableFiles);
     }
-    return { getRunFiles };
+    function loadFile(filename: string) {
+      console.log('loading'.concat(filename));
+    }
+    return {
+      getRunFiles,
+      availableFiles,
+      loadFile,
+    };
   },
 });
 </script>
