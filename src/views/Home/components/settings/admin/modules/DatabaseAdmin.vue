25d11297ad8e9b850ef15b8c2f35cf26105ac422
Jonahs11 <jonahsilverman11@gmail.com>
2022-09-28 15:42:43 -0400

    Update DatabaseAdmin.vue

--- src/views/Home/components/settings/admin/modules/DatabaseAdmin.vue
+++ src/views/Home/components/settings/admin/modules/DatabaseAdmin.vue
@@ -1,8 +1,10 @@
 <template>
+  <v-container>
     <v-row>
         <v-col>
             <v-btn
             color="primary"
+            :loading="button_loading"
             @click="repopulate_db"
             outlined
             >
@@ -10,6 +12,7 @@
             </v-btn>
         </v-col>
     </v-row>
+  </v-container>
 </template>
 
 <script lang='ts'>
@@ -19,6 +22,7 @@ import lodash from 'lodash';
 import store from '@/store';
 import { snackbar } from '@/components/GlobalSnackbar';
 import { generateRouteByQuery } from '@/utils';
+import { SnackbarParams } from '@/components/GlobalSnackbar/state';
 
 const clientReady = new Promise((resolve) => {
   const ready = computed(() => (
@@ -37,9 +41,17 @@ export default defineComponent({
     const currentRoute = computed(() => ctx.root.$route);
     const button_loading = ref<boolean>(false);
     async function repopulate_db() {
+      button_loading.value = true;
       console.log('repopulating DB');
       const value = await client.value?.repopulateDB();
-      button_loading.value = true;
+      if (value === 'Success') {
+        console.log('Success');
+        snackbar.dispatch({ text: 'Database Successfully Updated', options: { right: true, color: 'success', timeout: 10000 } });
+      } else {
+        snackbar.dispatch({ text: 'Database Updating Failed', options: { right: true, color: 'error', timeout: 10000 } });
+      }
+      console.log(value);
+      button_loading.value = false;
     }
     onMounted(async () => {
       await clientReady;
@@ -54,5 +66,9 @@ export default defineComponent({
 </script>
 
 <style>
+
+.repopulate_btn {
+  left: 5px;
+}
 
 </style>
