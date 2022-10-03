a2f4e13e0d6d6e2f4ded799e2f46cd8661352a53
Jonahs11 <jonahsilverman11@gmail.com>
2022-10-03 11:55:57 -0400

    Added functionality for adding and removing user groups

--- src/views/Home/components/settings/admin/modules/UserManagement.vue
+++ src/views/Home/components/settings/admin/modules/UserManagement.vue
@@ -3,7 +3,7 @@
         <v-row>
             <v-col>
                 <h2> User List </h2>
-                <p v-for="user in user_list" :key="user.id">
+                <p v-for="(user, index) in user_list" :key="index">
                     <v-btn
                     @click="user_selected(user)"
                     >
@@ -29,9 +29,10 @@
             >
             </v-text-field>
             <p>
-             Status: {{ selected_user.status }}
+             Status: {{ confirmation_status }}
              <v-btn
-             v-if="selected_user.status === 'UNCONFIRMED'"
+             v-if="confirmation_status === 'UNCONFIRMED'"
+             @click="confirm_user"
              >
              Confirm User
               </v-btn>
@@ -40,12 +41,13 @@
             v-model="users_groups"
             :items="groups_list"
             label="User's Groups"
-            @change="groups_list_changed"
+            @change="groups_list_changed()"
             multiple
             >
             </v-select>
             <v-btn
             v-if="changes_made"
+            @click="write_changes"
             >
               Confirm Changes
             </v-btn>
@@ -67,6 +69,7 @@
 <script lang='ts'>
 import { defineComponent, ref, watchEffect, computed, onMounted, watch } from '@vue/composition-api';
 import store from '@/store';
+import { add } from 'lodash';
 
 const clientReady = new Promise((resolve) => {
   const ready = computed(() => (
@@ -88,12 +91,49 @@ export default defineComponent({
     const number_new_groups_options = ref<number>(1);
     const selected_groups = ref<any[]>([]);
     const changes_made = ref<boolean>(false);
+    const confirmation_status = ref<string>('');
+    const confirm_user_status = ref<boolean>(false);
+    const removing_group_lis = ref<string[]>([]);
+    const adding_group_lis = ref<string[]>([]);
+    // const group_changes = new Map<string, boolean>();
+    const original_group_lis = ref<string[]>([]);
+
     function groups_list_changed() {
-      console.log('groups list');
+      adding_group_lis.value = users_groups.value.filter(
+        (val) => !original_group_lis.value.includes(val),
+      );
+      removing_group_lis.value = original_group_lis.value.filter(
+        (val) => !users_groups.value.includes(val),
+      );
+      if (removing_group_lis.value.length > 0 || adding_group_lis.value.length > 0 || confirm_user_status.value) {
+        changes_made.value = true;
+      } else {
+        changes_made.value = false;
+      }
     }
     function user_selected(user: any) {
+      changes_made.value = false;
       selected_user.value = user;
       users_groups.value = user.groups;
+      confirmation_status.value = user.status;
+      original_group_lis.value = users_groups.value;
+    }
+    function confirm_user() {
+      console.log('confirming user');
+      confirmation_status.value = 'CONFIRMED';
+      confirm_user_status.value = true;
+      changes_made.value = true;
+    }
+    function write_changes() {
+      if (confirm_user_status.value) {
+        console.log('confirming user status');
+      }
+      for (let i = 0; i < adding_group_lis.value.length; i += 1) {
+        console.log('adding to group '.concat(adding_group_lis.value[i]));
+      }
+      for (let i = 0; i < removing_group_lis.value.length; i += 1) {
+        console.log('removing from group '.concat(removing_group_lis.value[i]));
+      }
     }
     onMounted(async () => {
       await clientReady;
@@ -115,6 +155,13 @@ export default defineComponent({
       users_groups,
       changes_made,
       groups_list_changed,
+      confirm_user,
+      confirmation_status,
+      confirm_user_status,
+      original_group_lis,
+      removing_group_lis,
+      adding_group_lis,
+      write_changes,
     };
   },
 });
