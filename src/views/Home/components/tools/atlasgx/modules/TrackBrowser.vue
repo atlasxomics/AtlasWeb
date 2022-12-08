a7a26cb1d30178912606caa916397a89f5d59ffa
Jonahs11 <jonahsilverman11@gmail.com>
2022-12-08 16:55:29 -0500

    track fix

--- src/views/Home/components/tools/atlasgx/modules/TrackBrowser.vue
+++ src/views/Home/components/tools/atlasgx/modules/TrackBrowser.vue
@@ -155,7 +155,7 @@ export default defineComponent({
         },
       };
       const ref_bucket = 'atx-track-host';
-      const fl_payload = { path: 'data', filter: [`${rid}/tracks`], bucket: ref_bucket };
+      const fl_payload = { path: 'data', filter: [`${rid}/tracks`] };
       const fl_payload_ref = { bucket: ref_bucket, path: 'ref', filter: [`${speciesMap[selectedSpecies.value]}`] };
       const resp = await client.value.getFileList(fl_payload);
       resp.sort((a: any, b: any) => {
