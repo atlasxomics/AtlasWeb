6055d7eee91dccc610b4c3d157d7c283f543c30d
Jonahs11 <jonahsilverman11@gmail.com>
2023-01-04 13:27:44 -0500

    ^

--- src/views/Home/components/settings/admin/modules/CreateWebObject.vue
+++ src/views/Home/components/settings/admin/modules/CreateWebObject.vue
@@ -3,20 +3,18 @@
     <v-row>
       <v-col cols="12" sm="6">
         <v-card flat>
-          <v-card-title>Run ID</v-card-title>
           <v-card-actions>
+        <v-col>
+        <b> Create/Select Run ID </b>
         <run-id-selector
+          :new_available="true"
           @run-selected="run_id_selected"
           @edit-run-id="edit_run_id"
           @close-edit-run-id="run_id_selected_bool = true"
+          @custom-run-id="custom_run_id"
           >
         </run-id-selector>
-          </v-card-actions>
-          </v-card>
-      </v-col>
-      <v-col cols="12" sm="6">
-        <v-card flat>
-          <v-card-title>Path to H5AD File</v-card-title>
+        <b> Specify H5AD Path </b>
           <v-select
             :disabled="loading || !run_id_selected_bool"
             :loading="loading"
@@ -28,7 +26,6 @@
           <v-row>
           <v-text-field
           label="Path"
-          width="70%"
           :disabled="(loading || !bucket_name || path_selected || !run_id_selected_bool)"
           :loading="loading"
           v-model="path_name"
@@ -60,7 +57,7 @@
               </tbody>
             </template>
           </v-simple-table>
-          <v-checkbox :color="'green'" @click="toggle_transcriptome" value="is_transcriptome" v-if="(all_files.length > 0 && path_selected)" :disabled="!run_id_selected_bool" label="Is this run Transcriptome" />
+          <v-checkbox :color="'green'" @click="toggle_transcriptome"  v-model="is_transcriptome" v-if="(all_files.length > 0 && path_selected)" :disabled="!run_id_selected_bool" label="Is this run Transcriptome" />
           <v-data-table
             v-if="(bucket_name && !loading && !path_selected)"
             dense
@@ -70,18 +67,18 @@
             :items="search_consistent_paths"
             @click:row="select_path">
           </v-data-table>
-        </v-card>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col>
+            </v-col>
+          </v-card-actions>
+          </v-card>
         <v-btn
           :disabled="((!is_transcriptome || !genes_h5ad_present) && !all_files_present || !path_selected || loading || !bucket_name || !run_id_selected_bool || creation_job_sent)"
           style="position: relative; left: 45%; bottom: 2%;"
           @click="createObjects">
           Submit
         </v-btn>
-        <v-card flat
+      </v-col>
+      <v-col cols="12" sm="6">
+                <v-card flat
         class="mx-auto"
         v-if="run_id_selected_bool"
         >
@@ -103,6 +100,10 @@
         </jobs-table>
         </v-card-actions>
       </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
       </v-col>
     </v-row>
   </v-container>
@@ -187,7 +188,6 @@ export default defineComponent({
     }
     function toggle_transcriptome() {
       creation_job_sent.value = false;
-      is_transcriptome.value = !is_transcriptome.value;
       if (is_transcriptome.value === true) {
         all_files.value.forEach((file: any, index: number) => {
           if (file.name !== 'genes.h5ad') {
@@ -210,8 +210,16 @@ export default defineComponent({
       const one_string = important_objects.join();
       const allFileHold: any[] = [];
       fileNames.forEach((v: string) => {
-        if (one_string.includes(v)) allFileHold.push({ name: v, color: 'green', value: true, visible: true });
-        else allFileHold.push({ name: v, color: 'red', value: false, visible: true });
+        let obj: any = {};
+        if (one_string.includes(v)) {
+          obj = { name: v, color: 'green', value: true, visible: true };
+        } else {
+          obj = { name: v, color: 'red', value: false, visible: true };
+        }
+        if (is_transcriptome.value === true && v !== 'genes.h5ad') {
+          obj.visible = false;
+        }
+        allFileHold.push(obj);
       });
       all_files.value = allFileHold;
       if (one_string.includes('genes.h5ad') && one_string.includes('motifs.h5ad') && one_string.includes('motifs.csv')) {
@@ -233,7 +241,6 @@ export default defineComponent({
       all_files.value = [];
       path_selected.value = true;
       path_name.value = ev.path;
-      is_transcriptome.value = false;
       searchPath();
     }
     async function updateProgress(value: number) {
@@ -247,40 +254,49 @@ export default defineComponent({
     async function createObjects() {
       if (!client.value) return;
       try {
-        const task = 'webfile.create_files';
-        const queue = 'jonah_webfile';
-        const params = {
-          aws_path: `${path_name.value}/h5/obj`,
-          rna_flag: checkbox_flag.value,
-        };
-        const args: any[] = [params];
-        const kwargs = { run_id: run_id.value };
-        // const kwargs: any = { run_id: run_id.value };
-        const taskObject = await client.value.postTask(task, args, kwargs, queue);
-        update_jobs_table();
-        creation_job_sent.value = true;
-        await checkTaskStatus(taskObject._id);
-        /* eslint-disable no-await-in-loop */
-        while (taskStatus.value.status !== 'SUCCESS' && taskStatus.value.status !== 'FAILURE') {
-          if (taskStatus.value.status === 'PROGRESS') {
-            await updateProgress(taskStatus.value.progress);
-            progressMessage.value = `${taskStatus.value.progress}% - ${taskStatus.value.position}`;
-          }
-          await new Promise((r) => {
-            taskTimeout.value = window.setTimeout(r, 1000);
-          });
-          taskTimeout.value = null;
+        const ensure_created = await client.value?.ensure_run_id_created(run_id.value);
+        console.log(ensure_created);
+        if (ensure_created === 'Success') {
+          const task = 'webfile.create_files';
+          const queue = 'jonah_webfile';
+          const params = {
+            aws_path: `${path_name.value}/h5/obj`,
+            rna_flag: checkbox_flag.value,
+            bucket_name: bucket_name.value,
+          };
+          const args: any[] = [params];
+          const kwargs = { run_id: run_id.value };
+          // const kwargs: any = { run_id: run_id.value };
+          const taskObject = await client.value.postTask(task, args, kwargs, queue);
+          update_jobs_table();
+          creation_job_sent.value = true;
           await checkTaskStatus(taskObject._id);
-        }
-        if (taskStatus.value.status !== 'SUCCESS') {
-          snackbar.dispatch({ text: 'Worker failed in Creating Object', options: { right: true, color: 'error' } });
+          /* eslint-disable no-await-in-loop */
+          while (taskStatus.value.status !== 'SUCCESS' && taskStatus.value.status !== 'FAILURE') {
+            if (taskStatus.value.status === 'PROGRESS') {
+              await updateProgress(taskStatus.value.progress);
+              progressMessage.value = `${taskStatus.value.progress}% - ${taskStatus.value.position}`;
+            }
+            await new Promise((r) => {
+              taskTimeout.value = window.setTimeout(r, 1000);
+            });
+            taskTimeout.value = null;
+            await checkTaskStatus(taskObject._id);
+          }
+          if (taskStatus.value.status !== 'SUCCESS') {
+            snackbar.dispatch({ text: 'Worker failed in Creating Object', options: { right: true, color: 'error' } });
+            creation_job_sent.value = false;
+            loading.value = false;
+          } else {
+            snackbar.dispatch({ text: 'The Web Objects are created', options: { right: true, color: 'success' } });
+            update_jobs_table();
+            progressMessage.value = taskStatus.value.status;
+            const resp = taskStatus.value.result;
+          }
+        } else {
+          snackbar.dispatch({ text: 'Run ID not created', options: { right: true, color: 'error' } });
           creation_job_sent.value = false;
           loading.value = false;
-        } else {
-          snackbar.dispatch({ text: 'The Web Objects are created', options: { right: true, color: 'success' } });
-          update_jobs_table();
-          progressMessage.value = taskStatus.value.status;
-          const resp = taskStatus.value.result;
         }
       } catch (error) {
         console.log(error);
@@ -297,26 +313,33 @@ export default defineComponent({
       genes_h5ad_present.value = false;
       bucket_name.value = null;
     }
-    function run_id_selected(ev: any) {
+    async function run_id_selected(ev: any) {
       creation_job_sent.value = false;
-      const run_data = client.value?.get_info_from_run_id(ev);
-      run_data!.then(async (data) => {
-        console.log(data);
-        if (data[0].results_folder_path !== null) {
-          const path = data[0].results_folder_path;
-          const inx = path.indexOf('S3://') + 5;
-          const non_pre = path.substring(inx);
-          const inx2 = non_pre.indexOf('/');
-          const bucket = non_pre.substring(0, inx2);
-          const sub_path_name = non_pre.substring(inx2 + 1, non_pre.length - 1);
-          bucket_name.value = bucket;
-          await generatePaths();
-          path_name.value = sub_path_name;
-          select_path({ path: path_name.value });
-        }
-      });
+      reset_bucket_path();
+      const run_data = await client.value?.get_info_from_run_id(ev);
+      if (run_data[0].assay === 'Transcriptome') {
+        is_transcriptome.value = true;
+      }
+      if (run_data[0].results_folder_path !== null) {
+        const path = run_data[0].results_folder_path;
+        const inx = path.indexOf('S3://') + 5;
+        const non_pre = path.substring(inx);
+        const inx2 = non_pre.indexOf('/');
+        const bucket = non_pre.substring(0, inx2);
+        const sub_path_name = non_pre.substring(inx2 + 1, non_pre.length - 1);
+        bucket_name.value = bucket;
+        console.log(bucket_name);
+        await generatePaths();
+        path_name.value = sub_path_name;
+        select_path({ path: path_name.value });
+      }
       run_id.value = ev;
       run_id_selected_bool.value = true;
+    }
+    function custom_run_id(id: string) {
+      run_id_selected_bool.value = true;
+      run_id.value = id;
+      is_transcriptome.value = false;
       reset_bucket_path();
     }
     function edit_run_id() {
@@ -347,6 +370,7 @@ export default defineComponent({
       path_selected,
       fetchBuckets,
       searchPath,
+      custom_run_id,
       updatePath,
       generatePaths,
       createObjects,
