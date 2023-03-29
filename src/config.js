e01f8f922ac93fc19bc1e312d6a2f3e2dd5c3558
Jonahs11 <jonahsilverman11@gmail.com>
2023-03-29 15:19:08 -0400

    Included config file in git repo

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const config = {
+  atlasxbrowser: {
+    root_dir: 'Images',
+    root_dir_spatial: 'Images_spatial',
+    barcode_files_path: 'BarcodeFiles',
+    bucket_name: 'atx-illumina',
+    bucket_name_spatial: 'atx-illumina',
+    lims_available: true,
+    barcode_mapping: {
+      1: 'bc50v1-24.txt',
+      2: 'bc50v2-24.txt',
+      '1 (normal)': 'bc50v1-24.txt',
+      '2 (reverseB)': 'bc50v2-24.txt',
+    },
+  },
+};
+
+module.exports = config;
