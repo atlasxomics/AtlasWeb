a7af26048c1f3040b7cad893278c1a546a8b1564
Joshua Barnett <joshuab@atlasxomics.com>
2024-03-12 22:18:32 -0400

    Updated prodcution api

--- src/environment.ts
+++ src/environment.ts
@@ -1,5 +1,3 @@
 export const SERVER_URL = process.env.VUE_APP_SERVER_URL!;
 export const LOGIN_COOKIE = 'atxLogin';
-const url = window.location.href;
-const split = url.match(/https:.+\.custom\.pods\.latch\.bio/i);
-export const PROD_SERVER_URL = `${split}`;
+export const PROD_SERVER_URL = 'https://api.atlasxomics.com/production';
